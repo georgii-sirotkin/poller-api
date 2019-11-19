adf3321be9eeb6049730c4d5b58aba778dbddc90
Georgii Sirotkin <georgiysirotkin@gmail.com>
2019-11-19 11:22:55 -0500

    Add CORS headers. Add /auth/me endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,11 @@ use Illuminate\Http\Request;
 
 Route::middleware('web')->group(function () {
     Route::post('auth/login', 'Auth\LoginController@login');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('auth/me', 'Auth\GetCurrentUserController');
+    });
 });
+
+
 
